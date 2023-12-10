1cd8c8968e6abce301556edb860797c8baf4294e
Proxy Server <rootproxy@duck.com>
2023-12-10 09:17:59 +0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='caplogger',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='A custom logger for Capstone',
     install_requires=requirements,
