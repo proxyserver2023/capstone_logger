d729c1c35ac9f5de83be964ecf643d2ab6c3b584
Proxy Server <rootproxy@duck.com>
2023-12-10 20:31:28 +0600

    Updated readme

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-Install
+# Capstone Logger
 
+Capstone Logger is a Python logging library that provides advanced logging capabilities for your applications.
+
+## Installation
+
+You can install Capstone Logger from PyPI:
+
+```bash
+pip install capstone_logger
 ```
-pip install git+https://github.com/proxyserver2023/caplogger.git
+
+## Usage
+
+Here's a basic example of how to use Capstone Logger:
+
+```python
+from capstone_logger.logger import get_logger
+
+logger = get_logger(__name__)
+
+logger.info("This is an info message")
 ```
+
+## Contributing
+
+We welcome contributions! Please see our contributing guidelines for more details.
+
+## License
+
+Capstone Logger is licensed under the MIT License.
--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
 setup(
-    name='caplogger',
-    version='1.0.1',
-    packages=find_packages(),
-    description='A custom logger for Capstone',
-    install_requires=requirements,
+    name="capstone_logger",
+    version="0.0.1",
+    author="Author",
+    author_email="rootproxy@duck.com",
+    description="Logger for Capstone",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/proxyserver2023/capstone_logger",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+    install_requires=[
+        "python-json-logger>=2.0.7",
+        "python-logstash>=0.4.8",
+    ],
 )
